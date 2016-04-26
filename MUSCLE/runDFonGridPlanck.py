404da033ea74253b1fe963ba4e6e6238a5d67904
JohanComparat <johan.comparat@gmail.com>
2016-04-26 17:03:20 +0200

    s

--- MUSCLE/runDFonGridPlanck.py
+++ MUSCLE/runDFonGridPlanck.py
@@ -22,14 +22,15 @@ def prodBox(ng=254, boxsize=64):
 	return t1 - t0
 
 ti = time.time()
+print ti
 z = 0.0
 ng=272
-boxsize=64.
+boxsize=128.
 name = "Planck-ng"+str(ng)+"-L"+str(boxsize)
 tracer = 'lrg'
 path_to_outputCat = '/home2/jcomparat/LPTmeshes/'+name+'-'+tracer+'.fits.gz'
 
-#prodBox(ng, boxsize)
+prodBox(ng, boxsize)
 
 # global deduced parameters 
 Lbox = boxsize * uu.megaparsec
@@ -39,7 +40,7 @@ Mpart = massPerParticle.value
 DFunit = (aa.critical_density(z) * aa.Om(z)).to(uu.solMass/uu.megaparsec**3)
 
 # mesh parameters :
-dx = boxsize / (ng/2) # 1000/2048.
+dx = boxsize / (ng) # 1000/2048.
 cellVolume = (dx*uu.megaparsec)**3
 
 # conversion N particle to DF value
@@ -51,6 +52,7 @@ p32= cPickle.load(f)
 f.close()
 p33 = p32.reshape(ng**3,3)
 
+print time.time()
 # creation of the mesh
 meshsize = int(boxsize / dx) 
 DF = n.zeros((meshsize, meshsize, meshsize)) 
@@ -66,9 +68,7 @@ for ii in range(len(p33)):
 
 DF *= conversion
 
-n.mean(DF)
-n.max(DF)
-n.min(DF[DF>0])
+print time.time()
 
 # now opens the probability distribution of galaxies
 px = [8.281210709565847055e-04,
@@ -84,6 +84,51 @@ px = [8.281210709565847055e-04,
 2.280093715491228412e+00, 
 -7.436849283207645378e+00]
 
+# random assignment within the cell
+
+catalog=[]
+Rs = n.random.uniform(0,1,(meshsize, meshsize, meshsize))
+I, J, K = n.meshgrid(n.arange(meshsize), n.arange(meshsize), n.arange(meshsize))
+sel = (DF>threshold)
+
+proba = 10**n.polyval(px, n.log10(DF[sel]))
+Rs2 = n.random.uniform(0,1,len(proba))
+#lrg = (Rs[sel]<proba*10)
+lrg = (Rs2<proba)
+
+indices = n.transpose([ I[sel][lrg], J[sel][lrg], K[sel][lrg] ])
+
+minX = I[sel][lrg] * dx
+maxX = (I[sel][lrg]+1) * dx
+minY = J[sel][lrg] * dx
+maxY = (J[sel][lrg]+1) * dx
+minZ = K[sel][lrg] * dx
+maxZ = (K[sel][lrg]+1) * dx
+
+# from i,j,k to x,y,z
+catalog = n.empty((len(minX),4))
+for ll in range(len(minX)):
+	catalog[ll] = n.random.uniform(minX[ll],maxX[ll]), n.random.uniform(minY[ll],maxY[ll]), n.random.uniform(minZ[ll],maxZ[ll]), DF[sel][lrg][ll]
+	
+	
+"""
+threshold = 2.
+for ii in range(len(p33)):
+	index= (p33[ii]/dx).astype(int)
+	if len((index==meshsize).nonzero()[0])>0:
+		index[(index==meshsize)] = n.ones_like(index[(index==meshsize)])*(meshsize-1)
+		DFval = DF[index[0],index[1],index[2]]
+		if DFval < threshold:
+			pass
+	else :
+		DFval = DF[index[0],index[1],index[2]]
+		if DFval < threshold:
+			pass
+	if DFval >=index threshold and Rs[ii]<n.polyval(px, DFVal) :
+		catalog.append(n.hstack((p33[ii], DFval)))
+
+
+
 catalog=[]
 Rs = n.random.uniform(0,1,len(p33))
 threshold = 2.
@@ -92,12 +137,16 @@ for ii in range(len(p33)):
 	if len((index==meshsize).nonzero()[0])>0:
 		index[(index==meshsize)] = n.ones_like(index[(index==meshsize)])*(meshsize-1)
 		DFval = DF[index[0],index[1],index[2]]
+		if DFval < threshold:
+			pass
 	else :
 		DFval = DF[index[0],index[1],index[2]]
-	if DFval > threshold and Rs[ii]<n.polyval(px, DFVal) :
+		if DFval < threshold:
+			pass
+	if DFval >=index threshold and Rs[ii]<n.polyval(px, DFVal) :
 		catalog.append(n.hstack((p33[ii], DFval)))
 
-
+"""
 out = n.transpose(catalog)
 		
 c0 = fits.Column(name="DF",format='D', array=out[-1] )
